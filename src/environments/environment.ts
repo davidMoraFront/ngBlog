6ba39fafc123d08aa690c0b4dabf4ebe169e913f
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-03-18 00:31:37 +0100

    config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDVb3FkIpPpOWsA2trLpN4nLr6TZ_xgwek",
+    authDomain: "ngblog-1564a.firebaseapp.com",
+    databaseURL: "https://ngblog-1564a.firebaseio.com",
+    projectId: "ngblog-1564a",
+    storageBucket: "ngblog-1564a.appspot.com",
+    messagingSenderId: "820016732974",
+    appId: "1:820016732974:web:4335543252625e8fe9f8bf",
+    measurementId: "G-192G8STVVY"
+  }
 };
 
 /*
