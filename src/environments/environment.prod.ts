703fe4e860e7292dc5fbba9190c5cee21f099951
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-07-22 18:52:31 +0200

    deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDVb3FkIpPpOWsA2trLpN4nLr6TZ_xgwek",
+    authDomain: "ngblog-1564a.firebaseapp.com",
+    databaseURL: "https://ngblog-1564a.firebaseio.com",
+    projectId: "ngblog-1564a",
+    storageBucket: "ngblog-1564a.appspot.com",
+    messagingSenderId: "820016732974",
+    appId: "1:820016732974:web:4335543252625e8fe9f8bf",
+    measurementId: "G-192G8STVVY",
+  },
 };
